bd505242f42fcc89d58e6308a61febeb95dea662
pavanvan <pavangullapalli09@gmail.com>
2022-12-13 18:06:16 +0530

    Code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,33 +1,15 @@
 package com.driver;
 public class Main {
-public  static class RWOnly {
-    // Private member variable
-    private String name;
-
-    // Getter method for name
-    public String getName() {
-        return this.name;
-    }
-
-    // Setter method for name
-    public void setName(String name) {
-        this.name = name;
-    }
-}
-
-// Main.java
-
-
     public static void main(String[] args) {
         // Create an object of the RWOnly class
         RWOnly obj = new RWOnly();
 
         // Set the value of name using the setter method
-        obj.setName("Pavan gp");
+        obj.setName("Pavan");
 
         // Get the value of name using the getter method
-        String name = obj.getName();
 
-        System.out.println(name); // Output: "Pavan gp"
+              System.out.println(obj.getName());
+
     }
 }
